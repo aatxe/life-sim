34e7daf45251ba47f5c952d65f112b94e4a649c5
Aaron Weiss <aaronweiss74@gmail.com>
2015-05-18 19:12:34 -0400

    Added ChemoBody and redid biochem primitives.

--- backend/src/chem.rs
+++ backend/src/chem.rs
@@ -3,36 +3,39 @@ use std::collections::HashMap;
 use rand::{Rand, Rng};
 
 pub type Id = u8;
-pub type Concentration = f32;
-pub type ChemicalMap = HashMap<Id, Chemical>;
-pub type DeltaMap = HashMap<Id, Concentration>;
+pub type Concentration = u8;
+pub type LocusId = u8;
+pub type LocusValue = u8;
 
-pub trait ConcentrationExt {
-    fn clamp(&self, lo: Concentration, hi: Concentration) -> Concentration;
+pub struct ChemoBody {
+    chems: HashMap<Id, Chemical>
 }
 
-impl ConcentrationExt for Concentration {
-    fn clamp(&self, lo: Concentration, hi: Concentration) -> Concentration {
-        if *self > hi {
-            hi
-        } else if *self < lo {
-            lo
+impl ChemoBody {
+    pub fn new() -> ChemoBody {
+        ChemoBody { chems: HashMap::new() }
+    }
+
+    pub fn gain(&mut self, id: Id, amount: Concentration) -> bool {
+        let val = self.chems.entry(id).or_insert(Chemical::new(id));
+        if let Some(new) = val.concnt().checked_add(amount) {
+            *val = Chemical::with_concentration(id, new);
+            true
+        } else if val.concnt() == 255 {
+            false
         } else {
-            *self
+            *val = Chemical::with_concentration(id, 255);
+            false
         }
     }
-}
 
-pub trait ChemMapExt {
-    fn apply(&mut self, deltas: &DeltaMap);
-}
-
-impl ChemMapExt for ChemicalMap {
-    fn apply(&mut self, deltas: &DeltaMap) {
-        for (id, diff) in deltas.iter() {
-            let val = self.entry(*id).or_insert(Chemical::new(*id));
-            val.concentration += *diff;
-            val.concentration = val.concentration.clamp(0.0, 1.0);
+    pub fn lose(&mut self, id: Id, amount: Concentration) -> bool {
+        let val = self.chems.entry(id).or_insert(Chemical::new(id));
+        if let Some(new) = val.concnt().checked_sub(amount) {
+            *val = Chemical::with_concentration(id, new);
+            true
+        } else {
+            false
         }
     }
 }
@@ -46,7 +49,7 @@ pub struct Chemical {
 
 impl Chemical {
     pub fn new(id: Id) -> Chemical {
-        Chemical { id: id, concentration: 0.0 }
+        Chemical { id: id, concentration: 0 }
     }
 
     pub fn with_concentration(id: Id, concentration: Concentration) -> Chemical {
@@ -62,21 +65,38 @@ impl Chemical {
     }
 }
 
+
+#[derive_Rand]
+#[derive(RustcEncodable, RustcDecodable)]
+pub enum IoType {
+    Analogue,
+    Digital,
+}
+
 #[derive_Rand]
 #[derive(RustcEncodable, RustcDecodable)]
 pub struct Emitter {
+    kind: IoType,
     chemical: Id,
+    rate: u8,
     gain: Concentration,
+    locus: LocusId,
+    threshold: LocusValue,
+    clear_after_read: bool,
+    invert: bool,
 }
 
 impl Emitter {
-    pub fn new(chemical: Id, gain: Concentration) -> Emitter {
-        Emitter { chemical: chemical, gain: gain }
+    pub fn new(kind: IoType, chemical: Id, rate: u8, gain: Concentration, locus: LocusId,
+               threshold: LocusValue, clear_after_read: bool, invert: bool) -> Emitter {
+        Emitter { 
+            kind: kind, chemical: chemical, rate: rate, gain: gain, locus: locus, 
+            threshold: threshold, clear_after_read: clear_after_read, invert: invert,
+        }
     }
 
-    pub fn step(&self, deltas: &mut DeltaMap) {
-        let val = deltas.entry(self.chemical).or_insert(0.0);
-        *val += self.gain;
+    pub fn step(&self, body: &mut ChemoBody) {
+        unimplemented!()
     }
 }
 
@@ -113,112 +133,32 @@ impl Reaction {
         Reaction { kind: kind, rate: rate, tick: Cell::new(0) }
     }
 
-    pub fn step(&self, map: &ChemicalMap, deltas: &mut DeltaMap) {
-        self.tick.set(self.tick.get() + 1);
-        if self.tick.get() < self.rate { return }
-        self.tick.set(0);
-        match self.kind {
-            ReactionType::Normal(ref a, ref b, ref c, ref d) => {
-                let n = (map[&a.id].concentration / a.concentration)
-                        .min(map[&b.id].concentration / b.concentration); 
-                let mut update = |c: &Chemical, add: bool| {
-                    let val = deltas.entry(c.id).or_insert(0.0);
-                    if add {
-                        *val += n * c.concentration
-                    } else {
-                        *val -= n * c.concentration
-                    }
-                };
-                update(a, false);
-                update(b, false);
-                update(c, true);
-                update(d, true);
-            },
-            ReactionType::Fusion(ref a, ref b, ref c) => {
-                let n = (map[&a.id].concentration / a.concentration)
-                        .min(map[&b.id].concentration / b.concentration); 
-                let mut update = |c: &Chemical, add: bool| {
-                    let val = deltas.entry(c.id).or_insert(0.0);
-                    if add {
-                        *val += n * c.concentration
-                    } else {
-                        *val -= n * c.concentration
-                    }
-                };                
-                update(a, false);
-                update(b, false);
-                update(c, true);
-            },
-            ReactionType::Decay(ref a) => {
-                let n = map[&a.id].concentration / a.concentration;
-                let val = deltas.entry(a.id).or_insert(0.0);
-                *val -= n * a.concentration;
-            },
-            ReactionType::Catalytic(ref a, ref b, ref c) => {
-                let n = (map[&a.id].concentration / a.concentration)
-                        .min(map[&b.id].concentration / b.concentration); 
-                let mut update = |c: &Chemical, add: bool| {
-                    let val = deltas.entry(c.id).or_insert(0.0);
-                    if add {
-                        *val += n * c.concentration
-                    } else {
-                        *val -= n * c.concentration
-                    }
-                };
-                update(b, false);
-                update(c, true);
-            },
-            ReactionType::CatalyticBreakdown(ref a, ref b) => {
-                let n = (map[&a.id].concentration / a.concentration)
-                        .min(map[&b.id].concentration / b.concentration); 
-                let val = deltas.entry(b.id).or_insert(0.0);
-                *val -= n * b.concentration;
-            },
-        }
+    pub fn step(&self, body: &ChemoBody) {
+        unimplemented!()
     }
-}
-
-#[derive_Rand]
-#[derive(RustcEncodable, RustcDecodable)]
-pub enum ReceptorType {
-    /// Receptor triggers when concentration is below threshold.
-    LowerBound,
-    /// Receptor triggers when concentration is above threshold.
-    UpperBound,
 }
 
 #[derive_Rand]
 #[derive(RustcEncodable, RustcDecodable)]
 pub struct Receptor {
-    kind: ReceptorType,
+    kind: IoType,
     chemical: Id,
-    gain: f32,
+    nominal: LocusValue,
+    gain: LocusValue,
     threshold: Concentration,
+    invert: bool
 }
 
 impl Receptor {
-    pub fn new(kind: ReceptorType, chemical: Id, gain: f32, threshold: Concentration) -> Receptor {
-        Receptor { kind: kind, chemical: chemical, gain: gain, threshold: threshold }
-    }
-
-    pub fn step(&self, map: &mut ChemicalMap, deltas: &DeltaMap) -> Option<Concentration> {
-        let prev = map.entry(self.chemical).or_insert(Chemical::new(self.chemical)).concentration;
-        let curr = prev + deltas.get(&self.chemical).map(|u| *u).unwrap_or(0.0);
-        match self.kind {
-            ReceptorType::LowerBound => if prev > self.threshold && curr < self.threshold {
-                Some((curr * self.gain).clamp(0.0, 1.0))
-            } else {
-                None   
-            },
-            ReceptorType::UpperBound => if prev < self.threshold && curr > self.threshold {
-                Some((curr * self.gain).clamp(0.0, 1.0))
-            } else {
-                None   
-            },
+    pub fn new(kind: IoType, chemical: Id, nominal: LocusValue, gain: LocusValue,
+               threshold: Concentration, invert: bool) -> Receptor {
+        Receptor {
+            kind: kind, chemical: chemical, nominal: nominal, gain: gain, threshold: threshold,
+            invert: invert
         }
     }
 
-    pub fn id(&self) -> Id {
-        self.chemical
+    pub fn step(&self, body: &ChemoBody) {
+        unimplemented!()
     }
 }
