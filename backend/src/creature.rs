ee4f2256a1f187c097bf0721d1b81f3bbfb8ef65
Aaron Weiss <aaronweiss74@gmail.com>
2015-05-19 17:18:08 -0400

    Added death, aging, and drives from locus.

--- backend/src/creature.rs
+++ backend/src/creature.rs
@@ -30,4 +30,62 @@ impl Creature {
     pub fn chemo_body_mut(&self) -> RefMut<ChemoBody> {
         self.chem.borrow_mut()
     }
+
+    fn get(&self, id: Locus) -> LocusValue {
+        *self.loci.borrow_mut().entry(id as u8).or_insert(0)
+    }
+
+    pub fn is_alive(&self) -> bool {
+       self.get(Locus::Death) == 0
+    }
+
+    pub fn age(&self) -> Age {
+        if self.get(Locus::AgedToSenile) == 0 {
+            Age::Senile
+        } else if self.get(Locus::AgedToOld) == 0 {
+            Age::Old
+        } else if self.get(Locus::AgedToAdult) == 0 {
+            Age::Adult
+        } else if self.get(Locus::AgedToYouth) == 0 {
+            Age::Youth
+        } else if self.get(Locus::AgedToAdolescent) == 0 {
+            Age::Adolescent
+        } else if self.get(Locus::AgedToChild) == 0 {
+            Age::Child
+        } else {
+            Age::Baby
+        }
+    }
+
+    pub fn get_drive(&self, drive: Drive) -> LocusValue {
+        self.get(match drive {
+            Drive::Hunger => Locus::Hunger
+        })
+    }
+}
+
+#[repr(u8)]
+enum Locus {
+    Death            = 0,
+    AgedToChild      = 1,
+    AgedToAdolescent = 2,
+    AgedToYouth      = 3,
+    AgedToAdult      = 4,
+    AgedToOld        = 5,
+    AgedToSenile     = 6,
+    Hunger           = 7,
+}
+
+pub enum Age {
+    Baby,
+    Child,
+    Adolescent,
+    Youth,
+    Adult,
+    Old,
+    Senile
+}
+
+pub enum Drive {
+    Hunger
 }
