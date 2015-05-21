7fa15d10fd85623fabcad30dc4821c58a476778a
Aaron Weiss <aaronweiss74@gmail.com>
2015-05-20 22:01:18 -0400

    Fixed evolution conditions.

--- backend/src/bin/evolve.rs
+++ backend/src/bin/evolve.rs
@@ -7,13 +7,13 @@ fn main() {
     let mut creature = Creature::new();
     genome.init(&mut creature);
     let mut steps = 0;
-    while steps < 300 || creature.age() == Age::Baby {
+    while steps < 300 || creature.age() != Age::Child {
+        steps = 0;
         while steps < 300 {
             genome.step(&mut creature);
             steps += 1;
         }
         genome.mutate();
-        steps = 0;
     }
     genome.save("evolved.json").unwrap();
 }
